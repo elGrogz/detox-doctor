52d9e2673e44920de1a46136761188776a174a42
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-12-10 15:34:41 +0100

    created separate shell profile checker

--- operatingSystemChecks/macos.js
+++ operatingSystemChecks/macos.js
@@ -1,26 +1,14 @@
 import fs from "fs";
 import chalk from "chalk";
 import { execSync } from "child_process";
-import NodeDetector from "../utils/node-checker.js";
+import NodeDetector from "../utils/nodeChecker.js";
+import ShellProfileChecker from "../utils/shellProfileChecker.js";
 
 const macOsCheck = async () => {
-  const javahomeEnvVariable = "export JAVA_HOME=`/usr/libexec/java_home`";
-  const androidHomeEnvVariable =
-    "export ANDROID_HOME=$HOME/Library/Android/sdk";
-  const androidEmulatorVariable = "$ANDROID_HOME/emulator";
-  const androidSdkManagerVariable = "$ANDROID_HOME/tools/bin/sdkmanager";
-  const androidPlatformToolsVariable = "$ANDROID_HOME/platform-tools";
-  const androidCommandLineToolsVariable =
-    "$ANDROID_HOME/cmdline-tools/latest/bin";
-
-  const shell = process.env.SHELL;
-
   // MAIN SYSTEM CHECKS
   console.log(
     chalk.blueBright.bold.bgGreenBright("\nChecking Android and Java stuff:")
   );
-
-  // console.log(process.env);
 
   // Verify Node is installed
   NodeDetector.getNodeVersion();
@@ -52,108 +40,7 @@ const macOsCheck = async () => {
       "\nChecking system environmental variables:"
     )
   );
-
-  if (shell === "/bin/zsh" && fs.existsSync(`${process.env.HOME}/.zshrc`)) {
-    const zshrcContents = fs.readFileSync(
-      `${process.env.HOME}/.zshrc`,
-      "utf-8"
-    );
-
-    if (zshrcContents.includes(javahomeEnvVariable)) {
-      console.log(
-        chalk.green("✅ Shell profile contains Javahome:", javahomeEnvVariable)
-      );
-    } else {
-      console.log(
-        chalk.red(
-          "❌ Shell profile does not contain Javahome variable:",
-          javahomeEnvVariable
-        )
-      );
-    }
-
-    if (zshrcContents.includes(androidHomeEnvVariable)) {
-      console.log(
-        chalk.green(
-          "✅ Shell profile contains Android Home:",
-          androidHomeEnvVariable
-        )
-      );
-    } else {
-      console.log(
-        chalk.red(
-          "❌ Shell profile does not contain Android Home variable:",
-          androidHomeEnvVariable
-        )
-      );
-    }
-
-    if (zshrcContents.includes(androidEmulatorVariable)) {
-      console.log(
-        chalk.green(
-          "✅ Shell profile contains Android Emulator variable:",
-          androidEmulatorVariable
-        )
-      );
-    } else {
-      console.log(
-        chalk.red(
-          "❌ Shell profile does not contain Android Emulator variable:",
-          androidSdkManagerVariable
-        )
-      );
-    }
-
-    if (zshrcContents.includes(androidSdkManagerVariable)) {
-      console.log(
-        chalk.green(
-          "✅ Shell profile contains Android SDK Manager variable:",
-          androidSdkManagerVariable
-        )
-      );
-    } else {
-      console.log(
-        chalk.red(
-          "❌ Shell profile does not contain Android SDK Manager variable:",
-          androidSdkManagerVariable
-        )
-      );
-    }
-
-    if (zshrcContents.includes(androidPlatformToolsVariable)) {
-      console.log(
-        chalk.green(
-          "✅ Shell profile contains Android Platform Tools Variable:",
-          androidPlatformToolsVariable
-        )
-      );
-    } else {
-      console.log(
-        chalk.red(
-          "❌ Shell profile does not contain Android Platform Tools Variable:",
-          androidPlatformToolsVariable
-        )
-      );
-    }
-
-    if (zshrcContents.includes(androidCommandLineToolsVariable)) {
-      console.log(
-        chalk.green(
-          "✅ Shell profile contains Android Command Line Tools Variable:",
-          androidCommandLineToolsVariable
-        )
-      );
-    } else {
-      console.log(
-        chalk.red(
-          "❌ Shell profile does not contain Android Command Line Tools Variable:",
-          androidCommandLineToolsVariable
-        )
-      );
-    }
-  } else {
-    console.log("unexpected shell");
-  }
+  ShellProfileChecker.check();
 
   // Verify Android tools are working correctly
   console.log(
---
+++ utils/shellProfileChecker.js
@@ -0,0 +1,124 @@
+import fs from "fs";
+import chalk from "chalk";
+import { execSync } from "child_process";
+
+const javahomeEnvVariable = "export JAVA_HOME=`/usr/libexec/java_home`";
+const androidHomeEnvVariable = "export ANDROID_HOME=$HOME/Library/Android/sdk";
+const androidEmulatorVariable = "$ANDROID_HOME/emulator";
+const androidSdkManagerVariable = "$ANDROID_HOME/tools/bin/sdkmanager";
+const androidPlatformToolsVariable = "$ANDROID_HOME/platform-tools";
+const androidCommandLineToolsVariable =
+  "$ANDROID_HOME/cmdline-tools/latest/bin";
+
+const shell = process.env.SHELL;
+
+class ShellProfileChecker {
+  static check() {
+    if (shell === "/bin/zsh" && fs.existsSync(`${process.env.HOME}/.zshrc`)) {
+      const zshrcContents = fs.readFileSync(
+        `${process.env.HOME}/.zshrc`,
+        "utf-8"
+      );
+
+      if (zshrcContents.includes(javahomeEnvVariable)) {
+        console.log(
+          chalk.green(
+            "✅ Shell profile contains Javahome:",
+            javahomeEnvVariable
+          )
+        );
+      } else {
+        console.log(
+          chalk.red(
+            "❌ Shell profile does not contain Javahome variable:",
+            javahomeEnvVariable
+          )
+        );
+      }
+
+      if (zshrcContents.includes(androidHomeEnvVariable)) {
+        console.log(
+          chalk.green(
+            "✅ Shell profile contains Android Home:",
+            androidHomeEnvVariable
+          )
+        );
+      } else {
+        console.log(
+          chalk.red(
+            "❌ Shell profile does not contain Android Home variable:",
+            androidHomeEnvVariable
+          )
+        );
+      }
+
+      if (zshrcContents.includes(androidEmulatorVariable)) {
+        console.log(
+          chalk.green(
+            "✅ Shell profile contains Android Emulator variable:",
+            androidEmulatorVariable
+          )
+        );
+      } else {
+        console.log(
+          chalk.red(
+            "❌ Shell profile does not contain Android Emulator variable:",
+            androidSdkManagerVariable
+          )
+        );
+      }
+
+      if (zshrcContents.includes(androidSdkManagerVariable)) {
+        console.log(
+          chalk.green(
+            "✅ Shell profile contains Android SDK Manager variable:",
+            androidSdkManagerVariable
+          )
+        );
+      } else {
+        console.log(
+          chalk.red(
+            "❌ Shell profile does not contain Android SDK Manager variable:",
+            androidSdkManagerVariable
+          )
+        );
+      }
+
+      if (zshrcContents.includes(androidPlatformToolsVariable)) {
+        console.log(
+          chalk.green(
+            "✅ Shell profile contains Android Platform Tools Variable:",
+            androidPlatformToolsVariable
+          )
+        );
+      } else {
+        console.log(
+          chalk.red(
+            "❌ Shell profile does not contain Android Platform Tools Variable:",
+            androidPlatformToolsVariable
+          )
+        );
+      }
+
+      if (zshrcContents.includes(androidCommandLineToolsVariable)) {
+        console.log(
+          chalk.green(
+            "✅ Shell profile contains Android Command Line Tools Variable:",
+            androidCommandLineToolsVariable
+          )
+        );
+      } else {
+        console.log(
+          chalk.red(
+            "❌ Shell profile does not contain Android Command Line Tools Variable:",
+            androidCommandLineToolsVariable
+          )
+        );
+      }
+    } else {
+      console.log("unexpected shell");
+    }
+  }
+}
+
+export default ShellProfileChecker;
