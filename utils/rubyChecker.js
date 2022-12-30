345fb285967846e3517ef2eaf2986dbca0c30356
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-12-30 19:58:45 +0100

    added ruby checker

---
+++ utils/rubyChecker.js
@@ -0,0 +1,21 @@
+import { printSuccess, printLocation, printWarning } from "./logger.js";
+import { execSync } from "child_process";
+
+class DebugToolsChecker {
+  static checkFlipperInstallation() {
+    try {
+      const result = execSync("ruby -v");
+      const rubyVersion = result.toString();
+    } catch (error) {}
+
+    printWarning("Ruby installation not found");
+    return {
+      name: "Ruby Installation check",
+      success: false,
+      optional: false,
+      message: ``,
+    };
+  }
+}
+
+export default DebugToolsChecker;
