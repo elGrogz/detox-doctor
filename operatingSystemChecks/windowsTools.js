fecceaa4bdaa109412685d783dd8f24c51f77ab7
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-01-24 20:38:32 +0100

    added windows checks

--- operatingSystemChecks/windowsTools.js
+++ operatingSystemChecks/windowsTools.js
@@ -2,7 +2,39 @@ import OperatingSystemTools from "./operatingSystemTools.js";
 
 class WindowsTools extends OperatingSystemTools {
   runWindowsCheck() {
-    console.log("Windows check not available yet...");
+ // MAIN SYSTEM CHECKS
+ printCheckMessage("\nChecking Node installation");
+
+ // Verify Node is installed
+ this.runCheck(NodeDetector.getNodeInfo());
+
+ // Env vars check
+ printCheckMessage("\nChecking system environmental variables:");
+ const shellFileContents = ShellChecker.getShellContents(this.shell);
+
+ if (shellFileContents) {
+   this.runCheck(AndroidToolsChecker.checkJavaHomeEnvVar(shellFileContents));
+   this.runCheck(
+     AndroidToolsChecker.checkAndroidHomeEnvVar(shellFileContents)
+   );
+   this.runCheck(
+     AndroidToolsChecker.checkAndroidEmulatorEnvVar(shellFileContents)
+   );
+   this.runCheck(
+     AndroidToolsChecker.checkAndroidSdkManagerEnvVar(shellFileContents)
+   );
+   this.runCheck(
+     AndroidToolsChecker.checkAndroidPlatformToolsEnvVar(shellFileContents)
+   );
+   this.runCheck(
+     AndroidToolsChecker.checkAndroidCommandLineToolsEnvVar(
+       shellFileContents
+     )
+   );
+ }
+
+ return this.completedChecks;
+}
   }
 }
 
