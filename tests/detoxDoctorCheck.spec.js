b47234c0b90f1a4a33a437c2e1d5e8b79c7e7f67
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-02-01 17:36:03 +0100

    added couple of tests

--- tests/detoxDoctorCheck.spec.js
+++ tests/detoxDoctorCheck.spec.js
@@ -1,7 +1,21 @@
 import DetoxDoctorCheck from "../detoxDoctorCheck";
 
 describe("Detox Doctor Check", () => {
-  test("can run test", () => {
-    expect("hello").toEqual("hello");
+  test("contains no options if none are passed in", () => {
+    const detoxDoctorCheck = new DetoxDoctorCheck();
+
+    expect(detoxDoctorCheck.options).toEqual(undefined);
+  });
+
+  test("contains correct options if some are passed in", () => {
+    const options = {
+      iosOnly: true,
+      excludeOptional: true,
+    };
+
+    const detoxDoctorCheck = new DetoxDoctorCheck(options);
+
+    expect(detoxDoctorCheck.options.iosOnly).toBe(true);
+    expect(detoxDoctorCheck.options.excludeOptional).toBe(true);
   });
 });
