57349aa6bb78a6a7293480ef40c0202d38fc6d27
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-02-08 21:16:05 +0100

    improved result reporting

--- detoxDoctorCheck.js
+++ detoxDoctorCheck.js
@@ -72,38 +72,40 @@ class DetoxDoctorCheck {
   }
 
   reportOptionalActionsToTake(results) {
-    console.log(chalk.bgYellowBright("Optional Steps to take:"));
+    if (results) {
+      let filteredOptionalResults = results.filter(
+        (result) => result.optional === true && result.success === false
+      );
 
-    let filteredOptionalResults = results.filter(
-      (result) => result.optional === true && result.success === false
-    );
-
-    if (filteredOptionalResults.length > 0) {
-      filteredOptionalResults.forEach((result) => {
-        printWarning(chalk.yellow(result.message));
-      });
+      if (filteredOptionalResults.length > 0) {
+        console.log(chalk.bgYellowBright("Optional Steps to take:"));
+        filteredOptionalResults.forEach((result) => {
+          printWarning(chalk.yellow(result.message));
+        });
+      }
     } else {
-      console.log(chalk.green.underline("No optional steps to take!"));
+      console.log(chalk.bgGreen("\nNo optional steps to take!\n"));
     }
   }
 
   reportActionsToTake(results) {
-    console.log(chalk.bgRed("\nMandatory Steps to take:"));
-
-    let filteredResults = results.filter(
-      (result) => result.optional === false && result.success === false
-    );
-
-    if (filteredResults.length > 0) {
-      filteredResults.forEach((result) => {
-        printFail(chalk.red(result.message));
-      });
-    } else {
-      console.log(
-        chalk.green.underline(
-          "No mandatory steps to take! You are ready to use Detox"
-        )
+    if (results) {
+      let filteredResults = results.filter(
+        (result) => result.optional === false && result.success === false
       );
+
+      if (filteredResults.length > 0) {
+        console.log(chalk.bgRed("\nMandatory Steps to take:"));
+        filteredResults.forEach((result) => {
+          printFail(chalk.red(result.message));
+        });
+      } else {
+        console.log(
+          chalk.bgGreen(
+            "\nNo mandatory steps to take! You are ready to use Detox!"
+          )
+        );
+      }
     }
   }
 }
