ac3dd24c0a4bc7d42bfb1895941611a8fec3fbda
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-02-09 23:05:17 +0100

    test messing

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ program
   .description(
     "CLI tool to help you setup your local environment for running Detox tests"
   )
-  .version("1.0.32");
+  .version("1.0.33");
 
 program
   .command("check")
--- tests/detoxDoctorCheck.spec.js
+++ tests/detoxDoctorCheck.spec.js
@@ -1,6 +1,12 @@
 import DetoxDoctorCheck from "../detoxDoctorCheck";
 
+jest.mock("../detoxDoctorCheck");
+
 describe("Detox Doctor Check", () => {
+  beforeEach(() => {
+    DetoxDoctorCheck.mockClear();
+  });
+
   test("contains no options if none are passed in", () => {
     const detoxDoctorCheck = new DetoxDoctorCheck();
 
@@ -19,5 +25,7 @@ describe("Detox Doctor Check", () => {
     expect(detoxDoctorCheck.options.excludeOptional).toBe(true);
   });
 
-  test("start runs the correct os test", () => {});
+  test("start runs the correct os test", () => {
+    const check = new DetoxDoctorCheck();
+  });
 });
