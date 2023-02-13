0c330ebb2df65821ca8401bfe1410880eb60bd18
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-02-13 19:49:11 +0100

    simplified node checker

--- utils/nodeChecker.js
+++ utils/nodeChecker.js
@@ -1,33 +1,22 @@
 import { execSync } from "child_process";
-import { existsSync } from "fs";
 import { printSuccess, printLocation, printFail } from "./logger.js";
-
-const NODE_COMMON_PATHS = [
-  process.env._,
-  "/usr/local/bin/node",
-  "/opt/local/bin/node",
-];
 
 class NodeDetector {
   static getNodeInfo() {
     try {
-      const version = execSync("node -v").toString();
+      const nodeVersion = execSync("node -v").toString().replace("\n", "");
+      const nodeLocation = execSync("which node").toString().replace("\n", "");
 
-      const strippedVersion = version.replace("\n", "");
+      printSuccess(`Node version: ${printLocation(nodeVersion)}`);
+      printSuccess(`Node binary found at: ${printLocation(nodeLocation)}`);
 
-      for (let path of NODE_COMMON_PATHS) {
-        if (existsSync(path)) {
-          printSuccess(`Node version: ${printLocation(strippedVersion)}`);
-          printSuccess(`Node binary found at: ${printLocation(path)}`);
-          return {
-            name: "Node Version Check",
-            success: true,
-            optional: false,
-          };
-        }
-      }
+      return {
+        name: "Node Version Check",
+        success: true,
+        optional: false,
+      };
     } catch (error) {
-      printFail("Node binary could not be found in the expected locations");
+      printFail("Node binary could not be found in an expected location");
       return {
         name: "Node Version Check",
         success: false,
