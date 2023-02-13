3bcd3cf1011ae518e97d19fc568ea8697a2b2945
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-02-13 20:11:04 +0100

    whoops, should have made it 1.0.37

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ program
   .description(
     "CLI tool to help you setup your local environment for running Detox tests"
   )
-  .version("1.0.36");
+  .version("1.0.37");
 
 program
   .command("check")
